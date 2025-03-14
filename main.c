d6c6c633aae17ab008670fe5af827ba427a1c18f
Darkmoon1995 <Darkmoonmasih@gmail.com>
2025-03-14 23:51:37 +0330

    Added The screen It self (Hyped)

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+//#include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -26,32 +26,74 @@ char* apply( char* CommandTyped){
         Direcotory = (LoadDirectory(CurrentDirectory));
         return Direcotory;
     }
-    printf("%s",CommandTyped);
-    printf("\n Apply was called\n");
     return Direcotory;
 }
 
+void PrintLine(char* ToPrint, int* TextColor, int* BackGroundColor) {
+    TerminalSize Terminal_size;
+    get_terminal_size(&Terminal_size);
+    int width = Terminal_size.width;
+    int ToPrintLength = strlen(ToPrint);
+    for (int i = 0; i < width; i++) {
+        if (ToPrintLength < i) {
+            printf("\x1b[38;2;%i;%i;%im\x1b[48;2;%i;%i;%im ",TextColor[0], TextColor[1], TextColor[2],BackGroundColor[0], BackGroundColor[1], BackGroundColor[2]);}
+        else{
+            printf("\x1b[38;2;%i;%i;%im\x1b[48;2;%i;%i;%im%c", TextColor[0], TextColor[1], TextColor[2], BackGroundColor[0], BackGroundColor[1], BackGroundColor[2], ToPrint[i]);
+        }
+    }
 
+    // Reset the colors to default at the end of the line
+    printf("\x1b[0m");
+}
 
+void PrintScreen(char* TextToPrint,int CurrentLineNumber,int* LocationOfCursor, char* TheCommandBellow){
+    printf("\033[H\033[J");
+
+    int TextToPrintLength = strlen(TextToPrint);
+    TerminalSize Terminal_size;
+    get_terminal_size(&Terminal_size);
+    int height = Terminal_size.height;
+    char* Line[height];
+    int CurrentLine = 0;
+    for (int i = 0; i < TextToPrintLength; i++) {
+        if(TextToPrint[i] == '\n'){
+            CurrentLine++;
+        }
+        if (height + CurrentLineNumber - 2 > CurrentLine && CurrentLine > CurrentLineNumber  ) {
+            printf("%c",TextToPrint[i]);
+        }
+    }
+    int TextColor[3]; TextColor[0] = 255; TextColor[1] = 255; TextColor[2] = 255;
+    int BackGroundColor[3]; BackGroundColor[0] = 0; BackGroundColor[1] = 0; BackGroundColor[2] = 0;
+
+    PrintLine(TheCommandBellow,BackGroundColor ,TextColor);
+}
+char* CatDirectory(){
+    char* Awnser;
+    return Awnser;
+}
 //main
 int main() {
     char ch;
     char *CurrentWorkingDirectory;
     char CommandTyped[256];
     int CommandNumber = 0;
-    printf("%s", CommandTyped);
+    //printf("%s", CommandTyped);
     //TerminalSize Terminal_size; //Get it with Terminal_size.width and Terminal_size. height
-    printf("\033[H\033[J"); 
+    printf("\033[H\033[J");
+        printf("\033[?7l");
+        int *LocationOfCursor = 0;
 
     // Disable user input
     disable_input();
     while (1) {
-        
+        char *CurrentFile = read_file_as_string("/tmp/LastLocation");
+        PrintScreen(CurrentFile, 0 ,LocationOfCursor , CommandTyped);
         ch = getchar();
-        
+
         if(ch == '\n'){
             //If pressed Enter
-            
+
             CurrentWorkingDirectory = apply(CommandTyped);
             FreeFullString(CommandTyped);
             CommandNumber = 0;
@@ -61,7 +103,7 @@ int main() {
             //If presed Esc reset the command type for user to be able to write the next command
             FreeFullString(CommandTyped);
             CommandNumber = 0;
-            printf("\033[H\033[J"); 
+            printf("\033[H\033[J");
         }else{
             if(ch == 127){
                 CommandTyped[CommandNumber - 1] = '\0';
@@ -71,10 +113,9 @@ int main() {
                 CommandTyped[CommandNumber] = ch;            //If pressed Enter
 
                 CommandNumber++;
-                printf("%c",ch);
             }
         }
-        
+
     }
 
     // Re-enable user input before exiting
